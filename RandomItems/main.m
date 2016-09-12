c8dde21439a793ede3ae98641f02b27a7b6a101a
Princess Sampson <ps@kingonomics.com>
2016-09-11 20:19:51 -0400

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Princess Sampson on 9/11/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
